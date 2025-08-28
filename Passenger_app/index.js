3f7f7ed7c19106dc8641f30dcfd8e5a04f62597f
Abe Pasion <pasionabe@gmail.com>
2025-08-27 20:41:24 -0400

    Implemented Save button

--- Passenger_app/index.js
+++ Passenger_app/index.js
@@ -1,62 +1,61 @@
 // document.getElementById("count-el").innerText = 2;
 
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch + secondBatch
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
 
-let humanDogRatio = 7
-let myAge = 22
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+// let humanDogRatio = 7
+// let myAge = 22
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
 
-let bonusPoint = 50 
-bonusPoint = bonusPoint + 50
-console.log(bonusPoint)
-bonusPoint = bonusPoint - 75
-console.log(bonusPoint)
-bonusPoint = bonusPoint + 45
-console.log(bonusPoint)
+// let bonusPoint = 50 
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+// bonusPoint = bonusPoint - 75
+// console.log(bonusPoint)
+// bonusPoint = bonusPoint + 45
+// console.log(bonusPoint)
 
-console.log(count)
+// console.log(count)
 
+// Laps Challenge
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function totalLaps() {
+//     let total = lap1 + lap2 + lap3
+//     console.log(total)
+// }
+
+// let lapsCompleted = 0
+
+// function addLap() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// addLap()
+// addLap()
+// addLap()
+// console.log(lapsCompleted)
+
+let countEl = document.getElementById("count-el")
 let passenger = 0
 
 function increment() {
-    console.log("button was clicked")
-    passenger = passenger += 1
-    console.log(passenger)
-    document.getElementById("count-el").innerText = passenger;
+    // console.log("button was clicked")
+    passenger = passenger + 1
+    countEl.innerText = passenger
 }
 
 function decrement() {
-    console.log("decrement button was clicked")
     passenger = passenger - 1
-    console.log(passenger)
-    document.getElementById('count-el').innerText = passenger;
+    countEl.innerText = passenger
 }
 
-// Laps Challenge
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
-
-function totalLaps() {
-    let total = lap1 + lap2 + lap3
-    console.log(total)
-
-    foreach
+function save() {
+    console.log(countEl)
 }
-
-let lapsCompleted = 0
-
-function addLap() {
-    lapsCompleted = lapsCompleted + 1
-}
-
-addLap()
-addLap()
-addLap()
-
-console.log(lapsCompleted)
 
 
