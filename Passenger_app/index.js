d9067cfc3e5fbec100b8b94139378df5f25db16b
Abe Pasion <pasionabe@gmail.com>
2025-08-27 20:04:11 -0400

    Calculating Human Dog Ag

--- Passenger_app/index.js
+++ Passenger_app/index.js
@@ -1,7 +1,13 @@
 // document.getElementById("count-el").innerText = 2;
 
-let count = 0
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+let humanDogRatio = 7
 let myAge = 22
 
+let myDogAge = myAge * humanDogRatio
+
 console.log(count)
-console.log(myAge)
+console.log(myDogAge)
