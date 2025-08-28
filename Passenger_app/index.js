09d84dbf9feef9843989333d44bd3d4729c3eb6c
Abe Pasion <pasionabe@gmail.com>
2025-08-27 20:09:34 -0400

    Reassigning BonusPoint

--- Passenger_app/index.js
+++ Passenger_app/index.js
@@ -2,12 +2,19 @@
 
 let firstBatch = 5
 let secondBatch = 7
-
 let count = firstBatch + secondBatch
+
 let humanDogRatio = 7
 let myAge = 22
-
 let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+
+let bonusPoint = 50 
+bonusPoint = bonusPoint + 50
+console.log(bonusPoint)
+bonusPoint = bonusPoint - 75
+console.log(bonusPoint)
+bonusPoint = bonusPoint + 45
+console.log(bonusPoint)
 
 console.log(count)
-console.log(myDogAge)
