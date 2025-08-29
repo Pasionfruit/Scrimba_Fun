ab7c164d398e6a99abf9caee94b28a1e45210554
Abe Pasion <pasionabe@gmail.com>
2025-08-28 20:11:26 -0400

    Points challenge

--- AI_challengers/index.js
+++ AI_challengers/index.js
@@ -1,6 +1,26 @@
 let firstName = "Abe"
 let lastName = "Pasion"
+let name = firstName + " " + lastName
 
-let name = firstName + " " + lastName 
+let greeting = "Hi there,"
 
-console.log(name)
+function greets () {
+    let greetings = greeting + " " + firstName
+    console.log(greetings)
+}
+
+greets()
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Point() {
+    myPoints -= 1
+}
+
+add3Points()
+remove1Point()
+console.log(myPoints)
