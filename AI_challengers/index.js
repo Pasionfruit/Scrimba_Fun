68719607ac25eed6364d79cd4fab6c310ea2e5f7
Abe Pasion <pasionabe@gmail.com>
2025-08-28 21:03:26 -0400

    Calculator challenge

--- AI_challengers/index.js
+++ AI_challengers/index.js
@@ -1,42 +1,35 @@
-// let firstName = "Abe"
-// let lastName = "Pasion"
-// let name = firstName + " " + lastName
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
 
-// let greeting = "Hi there,"
 
-// function greets () {
-//     let greetings = greeting + " " + firstName
-//     console.log(greetings)
-// }
-
-// greets()
-
-// let myPoints = 3
-
-// function add3Points() {
-//     myPoints += 3
-// }
-
-// function remove1Point() {
-//     myPoints -= 1
-// }
-
-// add3Points()
-// remove1Point()
-// console.log(myPoints)
-
-// console.log("2" + 2) //22
-// console.log(11 + 7) //18
-// console.log(6 + "5") //65
-// console.log("My points: " + 5 + 9) //59
-// console.log(2 + 2) // 4
-// console.log("11" + "14") // 1114
-
-let errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase () {
-    console.log("button was clicked")
-    errorParagraph.textContent = "Something went wrong, please try again"
-
+function add() {
+    let ans = num1 + num2
+    sumEl.textContent = "Sum: " + ans
 }
+
+function subtract() {
+    let ans = num1 - num2
+    sumEl.textContent = "Sum: " + ans
+}
+
+function divide() {
+    let ans = num1 / num2
+    sumEl.textContent = "Sum: " + ans
+}
+
+function multiply() {
+    let ans = num1 * num2
+    sumEl.textContent = "Sum: " + ans
+}
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+
