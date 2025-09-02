2b33f7fb3e6f6f881a434807fc98be82f537dcc4
Abe Pasion <pasionabe@gmail.com>
2025-09-02 11:23:44 -0400

    Implements bidding logic

--- Blackjack_Game/index.js
+++ Blackjack_Game/index.js
@@ -55,24 +55,32 @@ function renderGame() {
     }
 
     houseEl.textContent = "House Cards: "
-    console.log(houseEl)
     for (let i = 0; i < houseCards.length; i++){
         houseEl.textContent += houseCards[i] + " "
     }
-    
+
+    // Bid logic
+    totalBid = 10
+    player.chips -= totalBid
+
+    bidEl.textContent = "bid: $" + totalBid
     sumEl.textContent = "Sum: " + sum
     houseSumEl.textContent = "House Sum: " + houseSum
+    playerEl.textContent = player.name + ": $" + player.chips
 
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        player.chips += (totalBid * 2)
+        playerEl.textContent = player.name + ": $" + player.chips
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
 
+    play
     messageEl.textContent = message
 }
 
